96eadd3574b9d44f091e69395bcf94a6903ef5e5
akoh.yokohama <akoh.acu.ss12@gmail.com>
2025-07-29 21:01:18 +0900

    RVS AS OF 29JUL

--- app/Http/Controllers/DiaryController.php
+++ app/Http/Controllers/DiaryController.php
@@ -7,6 +7,27 @@ use App\Models\Diary;
 
 class DiaryController extends Controller
 {
+    public function index()
+    {
+        // メッセージ一覧を取得
+        $diaries = Diary::all();         // 追加
+
+        // メッセージ一覧ビューでそれを表示
+        return view('diaries.index', [     // 追加
+            'diaries' => $diaries        // 追加
+        ]);                                 
+    }
+
+
+    public function create()
+    {
+        $diary = new Diary;
+
+        return view('diaries.create', [
+        'diary' => $diary,
+    ]);
+    }
+    
     public function store(Request $request)
     {
         // バリデーション
@@ -20,15 +41,8 @@ class DiaryController extends Controller
 
         $diary->save();
 
-        return redirect('/'); //質問：どこにreturnすべきか分かりません。
+        return redirect()->route('diaries.index'); 
     }
 
-    public function create()
-    {
-        $diary = new Diary;
-
-        return view('diaries.create', [
-        'diary' => $diary,
-    ]);
-    }
+    
 }
